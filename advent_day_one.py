f383e700e1673fcb0ed0f8105b4bec83523e6379
tylerjlivingston <66578104+tylerjlivingston@users.noreply.github.com>
2021-12-13 20:58:39 -0500

    Update advent_day_one.py
    Updated code to loop through desired measurements.

--- advent_day_one.py
+++ advent_day_one.py
@@ -1,32 +1,29 @@
 """
 Tyler Livingston
-December 9, 2021
+December 13, 2021
 Advent of Code: Day 1
 https://adventofcode.com/2021/day/1
 """
+import os
+import pandas as pd
 
-depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263] # Depths
+def get_measurement_data():
+    """Used to get the measurement data from csv."""
+    file = 'measurements'
+    project_location = os.path.dirname(
+        __file__)  # Current location of the project.
+    return pd.read_csv(os.path.join(project_location, (file + ".csv")))
 
-depth_temp = depths[0] # Set the depth temp to the first depth.
-increased_counter = 0
-decreased_counter = 0
-no_change_counter = 0
+increased_counter = 0 # Counter for increases.
+depth_temp = 263 # Value to start at.
 
-for depth in depths:
+for depth in get_measurement_data()["measurement"]:
     """Loop through the depths."""
     # print("Current Depth:",depth)
     if depth > depth_temp:
-        increased_counter += 1
-        print(f"{depth} (increased)")
-    elif depth < depth_temp:
-        decreased_counter += 1
-        print(f"{depth} (decreased)")
-    else: # depth == depth_temp
-       no_change_counter += 1
-       print(f"{depth} (no change)") 
+        # Current depth higher than previous depth.
+        increased_counter += 1 # Update the counter.
+        # print(f"{depth} (increased)")
     depth_temp = depth # Update the depth temp.
 
-print() # Used for formatting
-print(f"{increased_counter} Increase(s)")
-print(f"{decreased_counter} Decrease(s)")
-print(f"{no_change_counter} No Change(s)")
+print(f"Found: {increased_counter}")
