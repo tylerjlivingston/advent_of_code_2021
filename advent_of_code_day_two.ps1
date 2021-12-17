ddd8cafb8ad933c74e08403d2d80f831c2c9a713
tylerjlivingston <66578104+tylerjlivingston@users.noreply.github.com>
2021-12-16 20:17:07 -0500

    Add files via upload

---
+++ advent_of_code_day_two.ps1
@@ -0,0 +1,25 @@
+﻿<#
+Tyler Livingston
+Advent of Code - Day 2 Part 1
+https://adventofcode.com/2021/day/2
+#>
+
+$vertical_value = 0
+$forward_value = 0
+
+Import-Csv -Path "C:\users\tyjli\Desktop\puzzle_input.csv" | Foreach-Object { 
+    foreach ($direction in $_.PSObject.Properties) {
+        if ($direction.value -like 'forward *') {
+            $forward_value = $forward_value + [int]$direction.value.Replace("forward ", "")
+        }
+        if ($direction.value -like 'up *') {
+            $vertical_value = $vertical_value - [int]$direction.value.Replace("up ", "")
+        }
+        if ($direction.value -like 'down *') {
+            $vertical_value = $vertical_value + [int]$direction.value.Replace("down ", "")
+        }
+    }
+}
+
+$total = $forward_value * $vertical_value
+"Found Total: " + $total
